963921c1cfef296eeead4b34597deb4788f55de8
favian <lightbringerlal@gmail.com>
2024-02-01 10:54:43 +0200

    hm5

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProductCard = ({data}) => {
     return (
         <div className="container mx-auto mt-[2em] grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
-            {data.map((item) => <div className="border rounded-3xl p-3 shadow hover:shadow-2xl transition"> 
+            {data.map((item) => <div className="border rounded-3xl p-3 shadow hover:shadow-2xl transition" key={item.id}> 
                 <p>id: {item.id}</p>
                 <p>category: {item.category}</p>
                 <p>description: {item.description}</p>
